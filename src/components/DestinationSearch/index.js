40521094971b8e6b87a2030335573bf2fd019c70
bilalmohammed261 <126739937+bilalmohammed261@users.noreply.github.com>
2023-11-09 03:47:22 -0500

    commit with search implementation

--- src/components/DestinationSearch/index.js
+++ src/components/DestinationSearch/index.js
@@ -6,24 +6,45 @@ import './index.css'
 import DestinationItem from '../DestinationItem'
 
 class DestinationSearch extends Component {
+  state = {
+    searchInput: '',
+  }
+
+  onChangeInput = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
   render() {
     const {destinationsList} = this.props
+    // console.log(destinationsList)
 
-    console.log(destinationsList)
+    const {searchInput} = this.state
+    console.log(searchInput)
+
+    const searchedDestinationsList = destinationsList.filter(destination =>
+      destination.name.toLowerCase().includes(searchInput),
+    )
+    console.log(searchedDestinationsList)
 
     return (
       <div className="bg-container">
         <h1 className="title">Destination Search</h1>
         <div className="search-container">
-          <input type="search" placeholder="search" />
+          <input
+            type="search"
+            placeholder="search"
+            onChange={this.onChangeInput}
+            value={searchInput}
+          />
           <img
             src="https://assets.ccbp.in/frontend/react-js/destinations-search-icon-img.png"
-            alt="search"
+            alt="search icon"
+            className="icon"
           />
         </div>
 
         <ul className="list-container">
-          {destinationsList.map(destination => (
+          {searchedDestinationsList.map(destination => (
             <DestinationItem
               name={destination.name}
               imgUrl={destination.imgUrl}
